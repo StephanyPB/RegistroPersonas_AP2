523dd08488e2622a806eff4cff045a1a73066a4c
Stephany Paulino Balbi <stephany_paulino1@ucne.edu.do>
2022-05-09 21:51:45 -0400

    Creando los metodos para el proyecto

---
+++ app/src/main/java/com/example/registropersonas/data/UsuarioDao.kt
@@ -0,0 +1,31 @@
+package com.example.registropersonas.data
+
+import androidx.room.*
+import com.example.registropersonas.model.Persona
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface UsuarioDao {
+
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun Insertar(persona: Persona)
+
+    @Delete
+    suspend fun Eliminar(persona: Persona)
+
+
+    @Query("""
+        SELECT * 
+        FROM Personas
+        WHERE personaId=:personaId        
+    """)
+    fun Buscar(personaId: Int): Flow<Persona>
+
+    @Query("""
+        SELECT * 
+        FROM Personas
+        ORDER BY personaId    
+    """)
+    fun GetLista(): Flow<List<Persona>>
+}

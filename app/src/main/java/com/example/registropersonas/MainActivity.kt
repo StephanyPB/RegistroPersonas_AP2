df82c5f760299c4a211f6e270e1c0dd5fafec688
Stephany Paulino Balbi <stephany_paulino1@ucne.edu.do>
2022-05-09 21:59:34 -0400

    Agregando funcionalidad a la ventana visual

--- app/src/main/java/com/example/registropersonas/MainActivity.kt
+++ app/src/main/java/com/example/registropersonas/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.registropersonas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import com.example.registropersonas.databinding.ActivityMainBinding
+import com.example.registropersonas.model.Persona
+import com.example.registropersonas.viewmodel.UsuarioViewModel
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: UsuarioViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.guardarButton.setOnClickListener {
+            viewModel.guardar(
+                Persona(
+                    0,
+                    binding.nombreEditText.text.toString(),
+                    binding.balanceEditText.floatValue()
+                )
+            )
+        }
+            viewModel.guardado.observe(this) {
+                if (it) {
+                    Snackbar.make(binding.balanceEditText, "Guardado Exitoso!", Snackbar.LENGTH_LONG).show()
+                }
+            }
     }
+        fun TextInputEditText.floatValue() = text.toString().toFloatOrNull() ?: 0.0f
 }

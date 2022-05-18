c28d57a4600197588c121913de08d758bfac459b
Stephany Paulino Balbi <stephany_paulino1@ucne.edu.do>
2022-05-18 16:23:53 -0400

    Mejoras en el viewModel

--- app/src/main/java/com/example/registropersonas/viewmodel/UsuariosViewModel.kt
+++ app/src/main/java/com/example/registropersonas/viewmodel/UsuariosViewModel.kt
@@ -7,16 +7,20 @@ import androidx.lifecycle.viewModelScope
 import com.example.registropersonas.data.UsuarioDao
 import com.example.registropersonas.model.Persona
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
+
 import javax.inject.Inject
 
-
 @HiltViewModel
-class UsuarioViewModel @Inject constructor(
-    val usuarioDao: UsuarioDao
-) : ViewModel(){
+class UsuariosViewModel  @Inject constructor(
+    val usuarioDao : UsuarioDao
+): ViewModel() {
 
-    private val _guardado = MutableLiveData(false)
+    val personas: Flow<List<Persona>>
+    get() = usuarioDao.GetLista()
+
+    public val _guardado = MutableLiveData(false)
     val guardado: LiveData<Boolean> get() = _guardado
 
     fun guardar(persona: Persona){
